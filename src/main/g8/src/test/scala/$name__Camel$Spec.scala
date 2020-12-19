ba26db03946a7bc790e3c4e494c5e10e2971cb91
Yamada Kenji <kenji.yamada@gmail.com>
2020-12-19 12:40:06 +0900

    add scalatest examples

--- src/main/g8/src/test/scala/$name__Camel$Spec.scala
+++ src/main/g8/src/test/scala/$name__Camel$Spec.scala
@@ -1,5 +1,7 @@
 package $organization$.$name;format="lower,word"$
 
+import org.scalatest._
+import matchers.should._
 import org.scalatest.funspec.AnyFunSpec
 
 class HelloWorldSpec extends AnyFunSpec {
@@ -10,3 +12,28 @@ class HelloWorldSpec extends AnyFunSpec {
   }
 }
 
+class SomeSpec extends AnyFunSpec with Matchers with GivenWhenThen {
+  val targetList = List(1, 1, 2, 3, 5, 8)
+
+  describe(s"On ${targetList}") {
+    describe("Finding the last element of a list") {
+      it("should equal 8") {
+        val result = Something.last(targetList)
+        result should equal (8)
+
+        info("That's all by info")
+        note("This is note")
+        markup("This is *also* mark up")
+        alert("This is alert")
+      }
+    }
+    describe("Finding the second last of a list") {
+      ignore("should equal 5") {
+
+      }
+    }
+    describe("Finding the Kth element of a list") {
+      it("should equal 2") (pending)
+    }
+  }
+}
